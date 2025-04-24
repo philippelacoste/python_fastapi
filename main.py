2ddd736a92ab40a66c10d35f6c561517357fb3ab
Philippe Lacoste <placoste@univ-tln.fr>
2025-04-24 10:38:06 +0200

    init fastapi script

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+import json
+import uvicorn
+
+if __name__ == "__main__":
+    # with open("./config/config.json", "r", encoding="utf-8") as f:
+    #     config = json.load(f)
+    uvicorn.run("main:app")
